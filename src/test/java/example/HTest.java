e4fb9dce0d34648ba170846267b1cdfea3bda410
Stepan <50651530+VermOwl@users.noreply.github.com>
2021-03-22 20:58:27 +0300

    test fail result

--- src/test/java/example/HTest.java
+++ src/test/java/example/HTest.java
@@ -1,12 +1,16 @@
 package example;
 import org.junit.Test;
 
+import static org.junit.Assert.assertTrue;
+
 public class HTest {
 
     @Test
     public void test(){
-        if (1 == 1) {
+        if (1 == 2) {
             boolean a = true;
+        } else {
+            assertTrue(false);
         }
     }
 }
